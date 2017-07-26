786d00aaed84993e8c9c3603d7a8aa8f962881c7
oohira <oohira>
2017-07-26 16:51:16 +0900

    データ整理

---
+++ Assets/Sound/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	Rigidbody rb;
+	Transform target;
+	Vector3 dir;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent< Rigidbody > ();
+		target = GameObject.FindWithTag ( "Player" ).transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( transform != target ){
+			;
+		}
+	}
+}
---
+++ Assets/Sound/Scripts/SoundCube.cs
@@ -0,0 +1,23 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCube : MonoBehaviour {
+
+	public float speed;
+	public new KeyCode name;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (name) && Input.GetKey (KeyCode.Keypad8)) {
+			this.transform.position += this.transform.up * speed;
+		}
+		if (Input.GetKey (name) && Input.GetKey (KeyCode.Keypad2)) {
+			this.transform.position -= this.transform.up * speed;
+		}
+	}
+}
