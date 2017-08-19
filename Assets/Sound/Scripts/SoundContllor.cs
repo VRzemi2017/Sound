d9645e8ab5ae4356225b3792eba0ce1d28c76e07
W401-12\student <a>
2017-08-19 16:26:10 +0900

    ステレオ音源追加

---
+++ Assets/Sound/Scripts/SoundContllor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundContllor : MonoBehaviour {
+  
+    AudioSource audio;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.Pause();
+    }
+
+    private void UpDate()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            Debug.Log("MusicStart");
+            audio.UnPause();
+        }
+    }
+}
