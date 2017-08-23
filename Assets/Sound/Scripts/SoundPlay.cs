be96356e923639ec514a6d0697b77b534082745e
oohira <oohira>
2017-08-23 15:04:12 +0900

    3D音響と2D音響の入れ替えスクリプト作成
    ・Spaceボタンを押すと2D音響になります
    ・Return(Enter)ボタンを押すと3D音響になります

--- Assets/Sound/Scripts/SoundPlay.cs
+++ Assets/Sound/Scripts/SoundPlay.cs
@@ -41,5 +41,15 @@ public class SoundPlay : MonoBehaviour {
 			_soundCube.spatialize = true;
 			Debug.Log ( "空間化を有効にしました" );
 		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			_soundCube.spatialize = false;
+			_soundCube.spatialBlend = 0f;
+			Debug.Log ( "空間化を無効にし,3D→2D" );
+		}
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			_soundCube.spatialize = true;
+			_soundCube.spatialBlend = 1f;
+			Debug.Log ( "空間化を有効にし,2D→3D" );
+		}
 	}
 }
