8ac25cfa087aa5b3f2032a335e34ce1c52cc7d78
oohira <oohira>
2017-08-26 14:31:30 +0900

    Volume調整をするスクリプトの作成
    ・VolumeControllというスクリプトを作成しそこで調整できるようにしました

---
+++ Assets/Component/Sound/Scripts/VolumeControll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeControll : MonoBehaviour {
+
+	public AudioSource _soundCube1;
+
+	// Use this for initialization
+	void Start () {
+		_soundCube1 = GameObject.Find ("SoundCube1").GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F)) {
+			Debug.Log ( "Volumeを上げました" );
+			_soundCube1.volume += 0.1f;
+		}
+		if (Input.GetKeyDown (KeyCode.J)) {
+			Debug.Log ( "Volumeを下げました" );
+			_soundCube1.volume -= 0.1f;
+		}
+	}
+}
