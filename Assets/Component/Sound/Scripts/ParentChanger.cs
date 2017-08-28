17552d4154cfe7b598e1f2e1c0b736a2d41f151d
W401-12\student <a>
2017-08-28 17:19:39 +0900

    Soundテスト用スクリプトの作成
    ・SoundManagerというスクリプトで音量・２D３D音響の変更・一時停止などを操作できるようにしました
    ・ParentChangerというスクリプトで親子関係を変更できるようにしました

---
+++ Assets/Component/Sound/Scripts/ParentChanger.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentChanger : MonoBehaviour {
+
+
+
+	[System.Serializable]
+	public struct ParentData {
+		public GameObject _gameObject;
+		public GameObject _parent_gameObject;
+	}
+
+	public ParentData _music1;
+	public ParentData _music2;
+	public ParentData _music3;
+	public ParentData _music4;
+	public ParentData _music5;
+	public ParentData _music6;
+
+
+
+	public GameObject pa;
+	public GameObject pb;
+
+
+	// Use this for initialization
+	void Start () {
+		//親となるGameObjectを取得（デバック用）
+		pa = GameObject.Find ( "Cube" );
+		pb = GameObject.Find ( "Sphere" );
+
+		//ParentChangerのInspectorを初期化
+		_music1 = UpdateParentData ( _music1 );
+		_music2 = UpdateParentData ( _music2 );
+		_music3 = UpdateParentData ( _music3 );
+		_music4 = UpdateParentData ( _music4 );
+		_music5 = UpdateParentData ( _music5 );
+		_music6 = UpdateParentData ( _music6 );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//親子関係・positionを変えるイベント
+		if (Input.GetKey (KeyCode.A)) {
+			ChangeParent ( pa, _music1._gameObject );
+			MovePosition ( pa, _music1._gameObject );
+		}
+		if (Input.GetKey (KeyCode.B)) {
+			ChangeParent ( pb, _music1._gameObject );
+			MovePosition ( pb, _music1._gameObject );
+		}
+
+		//ParentChangerのInspectorを更新
+		_music1 = UpdateParentData ( _music1 );
+		_music2 = UpdateParentData ( _music2 );
+		_music3 = UpdateParentData ( _music3 );
+		_music4 = UpdateParentData ( _music4 );
+		_music5 = UpdateParentData ( _music5 );
+		_music6 = UpdateParentData ( _music6 );
+	}
+
+
+
+	//---obj1を親、obj2を子の関係にする関数
+	void ChangeParent( GameObject obj1, GameObject obj2 ) {
+		obj2.transform.parent = obj1.transform;
+	}
+
+
+	//---obj2のpositionをobj1のpositonに対応した位置に移動させる関数
+	void MovePosition( GameObject obj1, GameObject obj2 ) {
+		obj2.transform.position = obj1.transform.position;
+	}
+
+	//---ParentChangerのInspectorを更新する関数
+	ParentData UpdateParentData( ParentData x ) {
+		if (x._gameObject) {
+			x._parent_gameObject = x._gameObject.transform.parent.gameObject;
+		} else {
+			Debug.LogWarning (x + "にGameObjectがセットされていません");
+		}
+		return x;
+	}
+
+
+	//---ParentChangerのInspector内の情報を親子関係に反映させる関数 ※実行時Inspectorをいじくれないため未使用
+	void ReflectParentData( ParentData x ) {
+		if (x._gameObject) {
+			x._gameObject.transform.parent = x._parent_gameObject.transform;
+		}
+	}
+
+
+
+}
--- Assets/Component/Sound/Scripts/SoundManager.cs
+++ Assets/Component/Sound/Scripts/SoundManager.cs
@@ -4,46 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour {
-	/*
-	public AudioSource _soundObject1;
-	public float _soundObject1_volume;					//_soundObject1のVolumeを格納する変数
-	public bool _soundObject1_spatialize;				//_soundObject1のspatializeを格納する変数
-	public bool _soundObject1_PauseFlag;				//_soundObject1をPauseするか判定する変数
-
-	public AudioSource _soundObject2;
-	public float _soundObject2_volume;					//_soundObject2のVolumeを格納する変数
-	public bool _soundObject2_spatialize;				//_soundObject2のspatializeを格納する変数
-	public bool _soundObject2_PauseFlag;				//_soundObject2をPauseするか判定する変数
-
-	public AudioSource _soundObject3;
-	public float _soundObject3_volume;					//_soundObject3のVolumeを格納する変数
-	public bool _soundObject3_spatialize;				//_soundObject3のspatializeを格納する変数
-	public bool _soundObject3_PauseFlag;				//_soundObject3をPauseするか判定する変数
-
-	public AudioSource _soundObject4;
-	public float _soundObject4_volume;					//_soundObject4のVolumeを格納する変数
-	public bool _soundObject4_spatialize;				//_soundObject4のspatializeを格納する変数
-	public bool _soundObject4_PauseFlag;				//_soundObject4をPauseするか判定する変数
-
-	public AudioSource _soundObject5;
-	public float _soundObject5_volume;					//_soundObject5のVolumeを格納する変数
-	public bool _soundObject5_spatialize;				//_soundObject5のspatializeを格納する変数
-	public bool _soundObject5_PauseFlag;				//_soundObject5をPauseするか判定する変数
-
-	public AudioSource _soundObject6;
-	public float _soundObject6_volume;					//_soundObject6のVolumeを格納する変数
-	public bool _soundObject6_spatialize;				//_soundObject6のspatializeを格納する変数
-	public bool _soundObject6_PauseFlag;				//_soundObject6をPauseするか判定する変数
-*/
+	
 	[System.Serializable]
 	public struct SoundData {
 		public AudioSource _sound;
 		public float _sound_volume;					//_musicのVolumeを格納する変数
 		public bool _sound_spatialize;				//_musicのspatializeを格納する変数
 		public bool _sound_PauseFlag;				//_musicをPauseするか判定する変数
+		public bool _button_controll;				//各種設定をボタンでコントロールするか判定する変数
 	};
-
-
 	public SoundData _music1;
 	public SoundData _music2;
 	public SoundData _music3;
@@ -51,168 +20,53 @@ public class SoundManager : MonoBehaviour {
 	public SoundData _music5;
 	public SoundData _music6;
 
+	public float volume_change = 0.1f; 
+
+
+
 	// Use this for initialization
 	void Start () {
-		/*if (_soundObject1) {
-			_soundObject1_volume = _soundObject1.volume;
-			_soundObject1_spatialize = _soundObject1.spatialize;
-			_soundObject1_PauseFlag = true;
-		}*/
-
+		//SoundManagerのInspectorを初期化する
 		_music1 = InitializeAudioSource ( _music1._sound );
-		/*
-		if (_soundObject2) {
-			_soundObject2_volume = _soundObject2.volume;
-			_soundObject2_spatialize = _soundObject2.spatialize;
-			_soundObject2_PauseFlag = true;
-		}
-		if (_soundObject3) {
-			_soundObject3_volume = _soundObject3.volume;
-			_soundObject3_spatialize = _soundObject3.spatialize;
-			_soundObject3_PauseFlag = true;
-		}
-		if (_soundObject4) {
-			_soundObject4_volume = _soundObject4.volume;
-			_soundObject4_spatialize = _soundObject4.spatialize;
-			_soundObject4_PauseFlag = true;
-		}
-		if (_soundObject5) {
-			_soundObject5_volume = _soundObject5.volume;
-			_soundObject5_spatialize = _soundObject5.spatialize;
-			_soundObject5_PauseFlag = true;
-		}
-		if (_soundObject6) {
-			_soundObject6_volume = _soundObject6.volume;
-			_soundObject6_spatialize = _soundObject6.spatialize;
-			_soundObject6_PauseFlag = true;
-		}
-		*/
+		_music2 = InitializeAudioSource ( _music2._sound );
+		_music3 = InitializeAudioSource ( _music3._sound );
+		_music4 = InitializeAudioSource ( _music4._sound );
+		_music5 = InitializeAudioSource ( _music5._sound );
+		_music6 = InitializeAudioSource ( _music6._sound );
+	
 	}
 
 	// Update is called once per frame
 	void Update () {
-		//Inspector内での_soundObjectの調整
+		//Inspector内での調整を対応するAoudioSourceに反映する
 		_music1 = AdjustmentAudioSource( _music1 );
-		/*
-		if (_soundObject1) {
-			_soundObject1.volume = _soundObject1_volume;
-			_soundObject1.spatialize = _soundObject1_spatialize;
-			if (_soundObject1_PauseFlag) {
-				_soundObject1.Pause ();
-			} else {
-				_soundObject1.UnPause ();
-			}
-		}
-		if (_soundObject2) {
-			_soundObject2.volume = _soundObject2_volume;
-			_soundObject2.spatialize = _soundObject2_spatialize;
-			if (_soundObject2_PauseFlag) {
-				_soundObject2.Pause ();
-			} else {
-				_soundObject2.UnPause ();
-			}
-		}
-		if (_soundObject3) {
-			_soundObject3.volume = _soundObject3_volume;
-			_soundObject3.spatialize = _soundObject3_spatialize;
-			if (_soundObject3_PauseFlag) {
-				_soundObject3.Pause ();
-			} else {
-				_soundObject3.UnPause ();
-			}
-		}
-		if (_soundObject4) {
-			_soundObject4.volume = _soundObject4_volume;
-			_soundObject4.spatialize = _soundObject4_spatialize;
-			if (_soundObject4_PauseFlag) {
-				_soundObject4.Pause ();
-			} else {
-				_soundObject4.UnPause ();
-			}
-		}
-		if (_soundObject5) {
-			_soundObject5.volume = _soundObject5_volume;
-			_soundObject5.spatialize = _soundObject5_spatialize;
-			if (_soundObject5_PauseFlag) {
-				_soundObject5.Pause ();
-			} else {
-				_soundObject5.UnPause ();
-			}
-		}
-		if (_soundObject6) {
-			_soundObject6.volume = _soundObject6_volume;
-			_soundObject6.spatialize = _soundObject6_spatialize;
-			if (_soundObject6_PauseFlag) {
-				_soundObject6.Pause ();
-			} else {
-				_soundObject6.UnPause ();
-			}
-		}
-		*/
-
+		_music2 = AdjustmentAudioSource( _music2 );
+		_music3 = AdjustmentAudioSource( _music3 );
+		_music4 = AdjustmentAudioSource( _music4 );
+		_music5 = AdjustmentAudioSource( _music5 );
+		_music6 = AdjustmentAudioSource( _music6 );
 
 		//ボタンによる_soundObjectの調整
-		/*
-		if (_soundObject1) {
-			VolumeControll (_soundObject1);
-			SpatializeControll ( _soundObject1 );
-		}
-		if (_soundObject2) {
-			VolumeControll (_soundObject2);
-			SpatializeControll ( _soundObject2 );
-		}
-		if (_soundObject3) {
-			VolumeControll (_soundObject3);
-			SpatializeControll ( _soundObject3 );
-		}
-		if (_soundObject4) {
-			VolumeControll (_soundObject4);
-			SpatializeControll ( _soundObject4 );
-		}
-		if (_soundObject5) {
-			VolumeControll (_soundObject5);
-			SpatializeControll ( _soundObject5 );
-		}
-		if (_soundObject6) {
-			VolumeControll (_soundObject6);
-			SpatializeControll ( _soundObject6 );
-		}
-		*/
+		_music1 = ButtonControll( _music1 );
+		_music2 = ButtonControll( _music2 );
+		_music3 = ButtonControll( _music3 );
+		_music4 = ButtonControll( _music4 );
+		_music5 = ButtonControll( _music5 );
+		_music6 = ButtonControll( _music6 );
+
+
 	}
-
-
-
-
-
+		
 	void LateUpdate() {
-		//Inspectorに_soundObject1の調整を反映
-		_music1 = ReflectAudioSource( _music1._sound );
-		/*
-		if (_soundObject1) {
-			_soundObject1_volume = _soundObject1.volume;
-			_soundObject1_spatialize = _soundObject1.spatialize;
-		}
-		if (_soundObject2) {
-			_soundObject2_volume = _soundObject2.volume;
-			_soundObject2_spatialize = _soundObject2.spatialize;
-		}
-		if (_soundObject3) {
-			_soundObject3_volume = _soundObject3.volume;
-			_soundObject3_spatialize = _soundObject3.spatialize;
-		}
-		if (_soundObject4) {
-			_soundObject4_volume = _soundObject4.volume;
-			_soundObject4_spatialize = _soundObject4.spatialize;
-		}
-		if (_soundObject5) {
-			_soundObject5_volume = _soundObject5.volume;
-			_soundObject5_spatialize = _soundObject5.spatialize;
-		}
-		if (_soundObject6) {
-			_soundObject6_volume = _soundObject6.volume;
-			_soundObject6_spatialize = _soundObject6.spatialize;
-		}
-*/
+		//対応するAoudioSourceの値をInspector内に反映する
+		_music1 = ReflectAudioSource( _music1 );
+		_music2 = ReflectAudioSource( _music2 );
+		_music3 = ReflectAudioSource( _music3 );
+		_music4 = ReflectAudioSource( _music4 );
+		_music5 = ReflectAudioSource( _music5 );
+		_music6 = ReflectAudioSource( _music6 );
+
+
 	}
 
 
@@ -224,55 +78,32 @@ public class SoundManager : MonoBehaviour {
 
 
 
-	//---AudioSourceのVolumeを調整する関数
-	public void VolumeControll( AudioSource _music ) {
-		if (Input.GetKeyDown (KeyCode.F)) {
-			_music.volume += 0.1f;
-			Debug.Log ( _music.gameObject.name + "のVolumeを" + _music.volume + "にしました" );
-		}
-		if (Input.GetKeyDown (KeyCode.J)) {
-			_music.volume -= 0.1f;
-			Debug.Log ( _music.gameObject.name + "のVolumeを" + _music.volume + "にしました" );
-		}
-	}
 
-	//---AudioSourceのspatializeを調整する関数
-	public void SpatializeControll( AudioSource _music ) {
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			_music.spatialize = false;
-			_music.spatialBlend = 0f;
-			Debug.Log (_music.gameObject.name + "の空間化を無効にし,3D→2D");
-		}
-		if (Input.GetKeyDown (KeyCode.Return)) {
-			_music.spatialize = true;
-			_music.spatialBlend = 1f;
-			Debug.Log (_music.gameObject.name + "の空間化を有効にし,2D→3D");
-		}
-	}
+
+
 
 
 	//---SoundManagerのInspectorを初期化する関数
 	public SoundData InitializeAudioSource( AudioSource _music ) {
 		SoundData x;
-
+		//動的初期化
 		x._sound = null;
 		x._sound_volume = 0f;
 		x._sound_spatialize = false;
 		x._sound_PauseFlag = true;
+		x._button_controll = false;
 		if ( _music ) {
 			x._sound = _music;
 			x._sound_volume = _music.volume;
 			x._sound_spatialize = _music.spatialize;
-			x._sound_PauseFlag = true;
-			Debug.Log ( "初期化されたよ" );
-			return x;
+		} else {
+			Debug.LogWarning ("AudioSourceが対応してません。");
 		}
-		Debug.Log ( "何もないよ" );
 		return x;
 	}
 
 
-	//---Inspector内での調整をAoudioSourceに反映する関数
+	//---Inspector内での調整を対応するAoudioSourceに反映する関数
 	public SoundData AdjustmentAudioSource( SoundData x ) {
 		if (x._sound) {
 			x._sound.volume = x._sound_volume;
@@ -282,33 +113,77 @@ public class SoundManager : MonoBehaviour {
 			} else {
 				x._sound.UnPause ();
 			}
-			return x;
-
+		} else {
+			Debug.LogWarning (x + "に反映させるAudioSourceがありません");
 		}
-		Debug.LogError ( "何もないよ！！" );
 		return x;
 	}
 
 
-	//--AoudioSourceの値をInspector内に反映する関数
-	public SoundData ReflectAudioSource( AudioSource _music ) {
-		SoundData x;
-
-		x._sound = null;
-		x._sound_volume = 0f;
-		x._sound_spatialize = false;
-		x._sound_PauseFlag = true;
-		if (_music) {
-			x._sound_volume = _music.volume;
-			x._sound_spatialize = _music.spatialize;
-
-			return x;
+	//--対応するAoudioSourceの値をInspector内に反映する関数
+	public SoundData ReflectAudioSource( SoundData x ) {
+		if (x._sound) {
+			x._sound_volume = x._sound.volume;
+			x._sound_spatialize = x._sound.spatialize;
+		} else {
+			Debug.LogWarning (x + "にAudioSourceがないためInspectorに表示出来ません");
 		}
-		Debug.Log ("何もないよ");
 		return x;
 	}
 
 
+	//---AudioSourceのVolumeを調整する関数(ボタン操作)
+	public void VolumeControll( AudioSource _music ) {
+		if (Input.GetKey (KeyCode.V) && Input.GetKeyDown( KeyCode.UpArrow )) {
+			_music.volume += volume_change;
+			Debug.Log ( _music.gameObject.name + "のVolumeを" + _music.volume + "にしました" );
+		}
+		if (Input.GetKey (KeyCode.V) && Input.GetKeyDown( KeyCode.DownArrow )) {
+			_music.volume -= volume_change;
+			Debug.Log ( _music.gameObject.name + "のVolumeを" + _music.volume + "にしました" );
+		}
+	}
+
+	//---AudioSourceのspatializeを調整する関数(ボタン操作)
+	public void SpatializeControll( AudioSource _music ) {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (_music.spatialize) {
+				_music.spatialize = false;
+				_music.spatialBlend = 0f;
+				Debug.Log (_music.gameObject.name + "の空間化を無効にし,3D→2D");
+			} else {
+				_music.spatialize = true;
+				_music.spatialBlend = 1f;
+				Debug.Log (_music.gameObject.name + "の空間化を有効にし,2D→3D");
+			}
+		}
+	}
+
+
+	//---AudioSourceの再生・一時停止を操作する関数(ボタン操作) ※Inspector内のx._sound_PauseFlagを変更するだけです
+	public SoundData PauseControll( SoundData x ) {
+		if ( Input.GetKeyDown( KeyCode.Return ) ) {
+			if ( x._sound_PauseFlag ) {
+				x._sound_PauseFlag = false;
+				Debug.Log ( x._sound.gameObject.name + "を再生しました" );
+			} else {
+				x._sound_PauseFlag = true;
+				Debug.Log ( x._sound.gameObject.name + "を一時停止しました" );
+			}
+		}
+		return x;
+	}
+
+
+	//---ボタンによってAudioSourceの値を変化させる関数
+	public SoundData ButtonControll( SoundData x ) {
+		if (x._button_controll) {
+			VolumeControll (x._sound);			//Volume調整
+			SpatializeControll (x._sound);		//spatialize調整
+			x　= PauseControll( x );					//再生・一時停止を操作 ※仮引数に注意
+		}
+		return x;
+	}
 
 
 
